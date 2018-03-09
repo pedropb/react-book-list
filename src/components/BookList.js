9eaf5ad8769c8aa0272fba6778e70a7d2a4c8f69
Pedro Baracho <pedropb@trt3.jus.br>
2018-03-09 15:42:20 -0300

    Transforming BookList into a Class (stateful) component

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const BookList = () =>
-    (
-        <ul className='list-group col-sm-4'>
-            <li className='list-group-item'>Javascript: The Good Parts</li>
-            <li className='list-group-item'>Harry Potter</li>
-            <li className='list-group-item'>The Dark Tower</li>
-            <li className='list-group-item'>Eloquent Ruby</li>
-        </ul>
-    );
+class BookList extends Component {
+    render() {
+        return (
+            <ul className='list-group col-sm-4'>
+                <li className='list-group-item'>Javascript: The Good Parts</li>
+                <li className='list-group-item'>Harry Potter</li>
+                <li className='list-group-item'>The Dark Tower</li>
+                <li className='list-group-item'>Eloquent Ruby</li>
+            </ul>
+        );
+    }
+}
 
 export default BookList;
